4aa8859c80b420dc5f535bb2eec5201f074fffdd
Rahul Srivastava <rahul.srivastava@autodesk.com>
2023-08-10 14:15:56 +0530

    Test

--- TestOSS.cs
+++ TestOSS.cs
@@ -25,8 +25,8 @@ public class TestModelDerivativeAPi
     public static async Task ClassInitializeAsync(TestContext testContext)
     {
 
-     string clientId = Environment.GetEnvironmentVariable("CLIENT_ID") ?? "ClientId not found";
-    string clientSecret = Environment.GetEnvironmentVariable("CLIENT_SECRET") ?? "ClientSecret not found";
+     string clientId = Environment.GetEnvironmentVariable("Client_Id") ?? "ClientId not found";
+    string clientSecret = Environment.GetEnvironmentVariable("Client_Secret") ?? "ClientSecret not found";
 
     
     Console.WriteLine($"CLIENT_ID: {clientId}");
